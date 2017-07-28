851e16786a41630ffad7e35e5ffae848bcd1b4aa
nazmuddin77 <nazmuddinmavliwala@gmail.com>
2017-07-28 17:41:53 +0530

    Forgot password view created

---
+++ app/src/main/java/mavliwala/nazmuddin/zoloassignment/forgotpassword/views/ForgotPasswordActivity.java
@@ -0,0 +1,17 @@
+package mavliwala.nazmuddin.zoloassignment.forgotpassword.views;
+
+import mavliwala.nazmuddin.zoloassignment.R;
+import mavliwala.nazmuddin.zoloassignment.base.views.helpers.BaseActivity;
+
+/**
+ * Created by nazmuddinmavliwala on 28/07/17.
+ */
+
+public class ForgotPasswordActivity extends BaseActivity {
+
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_forgot_password;
+    }
+}
