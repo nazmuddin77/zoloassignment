94d51817239b6659481c183beb5dbd8ca0aa3334
nazmuddin77 <nazmuddinmavliwala@gmail.com>
2017-07-30 21:45:31 +0530

    removed redundant views

---
+++ app/src/main/java/mavliwala/nazmuddin/zoloassignment/app/di/identifiers/ScopedActivity.java
@@ -0,0 +1,16 @@
+package mavliwala.nazmuddin.zoloassignment.app.di.identifiers;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by nazmuddinmavliwala on 30/07/17.
+ */
+
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ScopedActivity {
+}
